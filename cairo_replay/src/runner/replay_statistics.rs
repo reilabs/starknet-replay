36181df83a0d1ec65c2d010f80ff1bc99be0adda
Eagle941 <8973725+Eagle941@users.noreply.github.com>
2024-05-28 16:23:27 +0100

    Added comments

--- cairo_replay/src/runner/replay_statistics.rs
+++ cairo_replay/src/runner/replay_statistics.rs
@@ -1,18 +1,33 @@
+//! The module which provides an interface to libfunc usage statistics.
+
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use smol_str::SmolStr;
 
 #[derive(Debug, Clone, Eq, PartialEq, Default)]
 pub struct ReplayStatistics {
+    /// This field contains the association between libfunc name (key) and
+    /// number of calls (value).
     pub concrete_libfunc: OrderedHashMap<String, usize>,
 }
 
 impl ReplayStatistics {
+    /// Initialisation of `ReplayStatistics`.
+    ///
+    /// The struct is initialised with field `concrete_libfunc` empty.
     pub fn new() -> Self {
         ReplayStatistics {
             concrete_libfunc: OrderedHashMap::default(),
         }
     }
 
+    /// Update `ReplayStatistics` with results from contract replay.
+    ///
+    /// Keys are added if new. If keys exist already, the value (count) is
+    /// increased accordingly.
+    ///
+    /// # Arguments
+    ///
+    /// - `input`: Input map of libfuncs.
     // TODO: Change in `OrderedHashMap<impl Into<String>, usize>`
     pub fn add_statistics(&mut self, input: &OrderedHashMap<SmolStr, usize>) {
         input.iter().for_each(|(libfunc, weight)| {
@@ -23,6 +38,13 @@ impl ReplayStatistics {
         });
     }
 
+    /// Update `self` with data in `from`.
+    ///
+    /// Same logic as for `self.add_statistics`.
+    ///
+    /// # Arguments
+    ///
+    /// - `from`: Input `ReplayStatistics` to get data from.
     pub fn merge(&mut self, from: &ReplayStatistics) {
         for (libfunc, weight) in from.concrete_libfunc.iter() {
             self.concrete_libfunc
