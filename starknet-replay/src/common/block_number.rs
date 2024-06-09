e85f14df4d58e214a38806203dbccd3beddad19b
Eagle941 <8973725+Eagle941@users.noreply.github.com>
2024-06-09 14:48:46 +0100

    Improved code documentation.

--- starknet-replay/src/common/block_number.rs
+++ starknet-replay/src/common/block_number.rs
@@ -1,13 +1,18 @@
+//! This is `starknet-replay` representation of a `Starknet` block number.
+
 use pathfinder_common::BlockNumber as PathfinderBlockNumber;
 
+/// `BlockNumber` is represented as a `u64` integer.
 #[derive(Copy, Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BlockNumber(u64);
 impl BlockNumber {
+    /// Creates a new `BlockNumber`.
     #[must_use]
     pub fn new(block_number: u64) -> Self {
         BlockNumber(block_number)
     }
 
+    /// Returns the block number as `u64`.
     #[must_use]
     pub fn get(&self) -> u64 {
         self.0
@@ -24,4 +29,3 @@ impl From<BlockNumber> for PathfinderBlockNumber {
         PathfinderBlockNumber::new_or_panic(val.0)
     }
 }
-// TODO: add `papyrus` block number
