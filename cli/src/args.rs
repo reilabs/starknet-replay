fe31db9e17b2e35acb2df3a1ce3935d14f038c73
Eagle941 <8973725+Eagle941@users.noreply.github.com>
2024-10-06 14:17:58 +0100

    Doc improvements
    Co-authored-by: Ara Adkins <iamrecursion@users.noreply.github.com>

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -48,10 +48,10 @@ pub struct Args {
 
     /// Set to perform serial replay of blocks.
     ///
-    /// Slower, but forces initial state of block `n+1` consistent with final
-    /// state of block `n`. This is not ensured with parallel replay because
-    /// the final state may differ with the final state on the official
-    /// blockchain.
+    /// Slower, but forces the initial state of block `n+1` to be consistent
+    /// with the final state of block `n`. This is not ensured with parallel
+    /// replay because the final state may differ with the final state
+    /// fetched from the chain.
     #[arg(long)]
     pub serial: bool,
 }
